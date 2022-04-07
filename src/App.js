269e4a9fb744f1371ea5b3e7da973fa68d075718
abimbolataofeek <abimbolaadewale9999@gmail.com>
2022-04-07 14:22:26 +0100

    woo-commerce project structure

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Navigation from './Components/Navigation/Navigation';
+import Homepage from './Components/Homepage/Homepage';
+import Commerce from './Components/Commerce/Commerce';
+import Develop from './Components/Develop/Develop';
+import Trusted from './Components/Trusted/Trusted';
+import Supported from './Components/Supported/Supported';
+import Footer from './Components/Footer/Footer';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <Navigation />
+    <Homepage />
+    <Commerce />
+    <Develop />
+    <Trusted />
+    <Supported/>
+    <Footer />
+    
+    </>
+  );
+}
+
+export default App;
